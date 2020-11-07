fce790547accc222aad1e1ad5147dea33855d7a7
Eduardo Torres C <edtorres.pe@gmail.com>
2020-11-07 16:14:10 -0500

    Se agrega cambios

--- enviro.php
+++ enviro.php
@@ -5,11 +5,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 // $dotenv = Dotenv\Dotenv::create(__DIR__);
 // $dotenv->load();
 
-use Dotenv\Dotenv;
+// use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+// $dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+
 $dotenv->load();
 
-$variablexD = getenv("APP_AA");
+$variablexD = $_SERVER["DB_HOSTNAME"];
 
-var_dump($variablexD);
+echo($variablexD);
